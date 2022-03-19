bb97c1f4f76b0970d83dc2f9997918d6d448f6aa
Sichan Choi <is528629@gmail.com>
2022-03-19 18:55:48 +0900

    불필요한 파일 제거 및 라이브러리 설치
    - @material-ui/core react-dom
    - react-dropzone
    - react-outside-click-handler
    - react-router-dom
    - react-scripts
    - react-zoom-pan-pinch
    - recoil
    - styled-components
    - use-sound

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { HomePage } from './pages/HomePage';
+import { ExamPage } from './pages/ExamPage';
+import { ResultPage } from './pages/ResultPage';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <RecoilRoot>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/test/count=:count/time=:time" element={<ExamPage />} />
+          <Route exact path="/result" element={<ResultPage />} />
+        </Routes>
+      </RecoilRoot>
+    </Router>
   );
-}
-
-export default App;
+};
